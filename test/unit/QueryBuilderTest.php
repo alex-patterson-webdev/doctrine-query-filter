a5ecd4153cb7955c8494931b371bba634b9091af
Alex Patterson <alex.patterson.webdev@gmail.com>
2021-02-13 14:39:44 +0000

    Additonal test coverage for QueryBuilder class

--- test/unit/QueryBuilderTest.php
+++ test/unit/QueryBuilderTest.php
@@ -7,6 +7,7 @@ namespace ArpTest\DoctrineQueryFilter;
 use Arp\DoctrineQueryFilter\QueryBuilder;
 use Arp\DoctrineQueryFilter\QueryBuilderInterface;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query\Expr;
 use Doctrine\ORM\QueryBuilder as DoctrineQueryBuilder;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
@@ -62,5 +63,97 @@ final class QueryBuilderTest extends TestCase
             ->willReturn($newDoctrineQueryBuilder);
 
         $this->assertInstanceOf(QueryBuilder::class, $queryBuilder->createQueryBuilder());
+    }
+
+    /**
+     * Assert that calls to getEntityManager will return the internal query builder's entity manager instance
+     */
+    public function testGetEntityManagerWillReturnTheConfiguredEntityManagerInstance(): void
+    {
+        $queryBuilder = new QueryBuilder($this->doctrineQueryBuilder);
+
+        /** @var EntityManager|MockObject $entityManager */
+        $entityManager = $this->createMock(EntityManager::class);
+
+        $this->doctrineQueryBuilder->expects($this->once())
+            ->method('getEntityManager')
+            ->willReturn($entityManager);
+
+        $queryBuilder->getEntityManager();
+    }
+
+    /**
+     * Assert that calls to expr() will return the internal query builder's Expr instance
+     */
+    public function testGetQueryBuilderWillReturnTheConfiguredExprInstance(): void
+    {
+        $queryBuilder = new QueryBuilder($this->doctrineQueryBuilder);
+
+        /** @var Expr|MockObject $expr */
+        $expr = $this->createMock(Expr::class);
+
+        $this->doctrineQueryBuilder->expects($this->once())
+            ->method('expr')
+            ->willReturn($expr);
+
+        $queryBuilder->expr();
+    }
+
+    /**
+     * Assert that calls to expr() will return the internal query builder's DQL parts
+     */
+    public function testGetEntityManagerWillReturnTheConfiguredExprInstance(): void
+    {
+        $queryBuilder = new QueryBuilder($this->doctrineQueryBuilder);
+
+        $parts = [
+            'foo' => 'bar',
+        ];
+
+        $this->doctrineQueryBuilder->expects($this->once())
+            ->method('getDQLParts')
+            ->willReturn($parts);
+
+        $this->assertSame($parts, $queryBuilder->getQueryParts());
+    }
+
+    /**
+     * Assert that calls to innerJoin() will proxy to the internal query builder
+     */
+    public function testInnerJoinWillProxyToInternalQueryBuilder(): void
+    {
+        $queryBuilder = new QueryBuilder($this->doctrineQueryBuilder);
+
+        $name = 'foo';
+        $alias = 'a';
+        $type = Expr\Join::ON;
+        $condition = '1 = 1';
+        $indexBy = null;
+
+        $this->doctrineQueryBuilder->expects($this->once())
+            ->method('innerJoin')
+            ->with($name, $alias, $type, $condition, $indexBy);
+
+        $this->assertSame($queryBuilder, $queryBuilder->innerJoin($name, $alias, $type, $condition, $indexBy));
+    }
+
+    /**
+     * Assert that calls to leftJoin() will proxy to the internal query builder
+     */
+    public function testLeftJoinWillProxyToInternalQueryBuilder(): void
+    {
+        $queryBuilder = new QueryBuilder($this->doctrineQueryBuilder);
+
+        $name = 'bar';
+        $alias = 'b';
+        $type = Expr\Join::ON;
+        $condition = 'a.test = b.hello';
+        $indexBy = 'a.name';
+
+        $this->doctrineQueryBuilder->expects($this->once())
+            ->method('leftJoin')
+            ->with($name, $alias, $type, $condition, $indexBy);
+
+        $this->assertSame($queryBuilder, $queryBuilder->leftJoin($name, $alias, $type, $condition, $indexBy));
     }
 }
