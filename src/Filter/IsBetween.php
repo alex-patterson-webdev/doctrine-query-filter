05b2e7fd2f3fed7aab5cd621f0245e120aeab1e0
Alex Patterson <alex.patterson.webdev@gmail.com>
2021-03-06 22:00:06 +0000

    Add new test case for IsBetween filtering

--- src/Filter/IsBetween.php
+++ src/Filter/IsBetween.php
@@ -14,7 +14,7 @@ use Arp\DoctrineQueryFilter\QueryBuilderInterface;
  * @author  Alex Patterson <alex.patterson.webdev@gmail.com>
  * @package Arp\DoctrineQueryFilter\Filter
  */
-final class IsBetween extends AbstractFilter
+class IsBetween extends AbstractFilter
 {
     /**
      * @param QueryBuilderInterface $queryBuilder
@@ -28,19 +28,18 @@ final class IsBetween extends AbstractFilter
     {
         if (empty($criteria['from'])) {
             throw new InvalidArgumentException(
-                sprintf('The required \'from\' criteria option is missing for filter \'%s\'', static::class)
+                sprintf('The required \'from\' criteria option is missing for filter \'%s\'', self::class)
             );
         }
 
         if (empty($criteria['to'])) {
             throw new InvalidArgumentException(
-                sprintf('The required \'to\' criteria option is missing for filter \'%s\'', static::class)
+                sprintf('The required \'to\' criteria option is missing for filter \'%s\'', self::class)
             );
         }
 
         $fieldName = $this->resolveFieldName($metadata, $criteria);
-
-        $queryAlias = (($criteria['alias'] ?? $this->options['alias']) ?? 'entity');
+        $queryAlias = $this->getAlias($queryBuilder, $criteria['alias'] ?? '');
 
         $fromParamName = $this->createParamName($queryAlias);
         $toParamName = $this->createParamName($queryAlias);
@@ -57,14 +56,16 @@ final class IsBetween extends AbstractFilter
             $queryBuilder->orWhere($expression);
         }
 
+        $format = $criteria['format'] ?? null;
+
         $queryBuilder->setParameter(
             $fromParamName,
-            $this->formatValue($metadata, $fieldName, $criteria['from'], $criteria['format'] ?? null)
+            $this->formatValue($metadata, $fieldName, $criteria['from'], $format)
         );
 
         $queryBuilder->setParameter(
             $toParamName,
-            $this->formatValue($metadata, $fieldName, $criteria['to'], $criteria['format'] ?? null)
+            $this->formatValue($metadata, $fieldName, $criteria['to'], $format)
         );
     }
 }
---
+++ test/unit/Filter/IsBetweenTest.php
@@ -0,0 +1,209 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArpTest\DoctrineQueryFilter\Filter;
+
+use Arp\DoctrineQueryFilter\Constant\WhereType;
+use Arp\DoctrineQueryFilter\Filter\Exception\FilterException;
+use Arp\DoctrineQueryFilter\Filter\Exception\InvalidArgumentException;
+use Arp\DoctrineQueryFilter\Filter\FilterInterface;
+use Arp\DoctrineQueryFilter\Filter\IsBetween;
+use Doctrine\ORM\Query\Expr;
+use PHPUnit\Framework\MockObject\MockObject;
+
+/**
+ * @covers  \Arp\DoctrineQueryFilter\Filter\IsBetween
+ * @covers  \Arp\DoctrineQueryFilter\Filter\AbstractFilter
+ *
+ * @author  Alex Patterson <alex.patterson.webdev@gmail.com>
+ * @package ArpTest\DoctrineQueryFilter\Filter
+ */
+final class IsBetweenTest extends AbstractFilterTest
+{
+    /**
+     * Assert that the class implements FilterInterface
+     */
+    public function testImplementsFilterInterface(): void
+    {
+        $filter = new IsBetween($this->queryFilterManager, $this->typecaster);
+
+        $this->assertInstanceOf(FilterInterface::class, $filter);
+    }
+
+    /**
+     * Assert that a InvalidArgumentException is thrown when attempting to call filter() without
+     * the required 'from' key
+     *
+     * @throws FilterException
+     * @throws InvalidArgumentException
+     */
+    public function testFilterWillThrowInvalidArgumentExceptionIfTheRequiredFromCriteriaIsMissing(): void
+    {
+        $filter = new IsBetween($this->queryFilterManager, $this->typecaster);
+
+        $criteria = [
+            // Missing 'from' key
+        ];
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            sprintf('The required \'from\' criteria option is missing for filter \'%s\'', IsBetween::class)
+        );
+
+        $filter->filter($this->queryBuilder, $this->metadata, $criteria);
+    }
+
+    /**
+     * Assert that a InvalidArgumentException is thrown when attempting to call filter() without
+     * the required 'to' key
+     *
+     * @throws FilterException
+     * @throws InvalidArgumentException
+     */
+    public function testFilterWillThrowInvalidArgumentExceptionIfTheRequiredToCriteriaIsMissing(): void
+    {
+        $filter = new IsBetween($this->queryFilterManager, $this->typecaster);
+
+        $criteria = [
+            'from' => '2021-03-01 00:00:00',
+            // Missing required 'to' key
+        ];
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            sprintf('The required \'to\' criteria option is missing for filter \'%s\'', IsBetween::class)
+        );
+
+        $filter->filter($this->queryBuilder, $this->metadata, $criteria);
+    }
+
+    /**
+     * @param array       $criteria
+     * @param string|null $whereType
+     * @param string|null $alias
+     * @param string|null $formatType
+     *
+     * @throws FilterException
+     * @throws InvalidArgumentException
+     *
+     * @dataProvider getFilterIsBetweenData
+     */
+    public function testFilterIsBetween(
+        array $criteria,
+        ?string $whereType = null,
+        ?string $alias = null,
+        ?string $formatType = null
+    ): void {
+        /** @var IsBetween|MockObject $filter */
+        $filter = $this->getMockBuilder(IsBetween::class)
+            ->setConstructorArgs([$this->queryFilterManager, $this->typecaster])
+            ->onlyMethods(['createParamName'])
+            ->getMock();
+
+        $rootAlias = 'entity';
+        $from = $criteria['from'] ?? '';
+        $to = $criteria['to'] ?? '';
+        $fieldName = $criteria['field'] = $criteria['field'] ?? 'test';
+
+        if (null !== $alias) {
+            $criteria['alias'] = $alias;
+        }
+
+        if (null !== $whereType) {
+            $criteria['where'] = $whereType;
+        }
+
+        if (null !== $formatType) {
+            $criteria['format'] = $formatType;
+        }
+
+        $this->metadata->expects($this->once())
+            ->method('hasField')
+            ->with($fieldName)
+            ->willReturn(true);
+
+        if (empty($alias)) {
+            $alias = $rootAlias;
+
+            $this->queryBuilder->expects($this->once())
+                ->method('getRootAlias')
+                ->willReturn($rootAlias);
+        }
+
+        /** @var Expr|MockObject $expr */
+        $expr = $this->createMock(Expr::class);
+
+        $this->queryBuilder->expects($this->once())
+            ->method('expr')
+            ->willReturn($expr);
+
+        $fromParam = $alias . 'abc123';
+        $toParam = $alias . 'zyx999';
+
+        $filter->expects($this->exactly(2))
+            ->method('createParamName')
+            ->withConsecutive(
+                [$alias],
+                [$alias]
+            )->willReturnOnConsecutiveCalls(
+                $fromParam,
+                $toParam
+            );
+
+        $isBetween = $alias . '.' . $fieldName . ' BETWEEN ' . $fromParam . ' AND ' . $toParam;
+        $expr->expects($this->once())
+            ->method('between')
+            ->with(
+                $alias . '.' . $fieldName,
+                ':' . $fromParam,
+                ':' . $toParam
+            )->willReturn(
+                $isBetween
+            );
+
+        if (null === $whereType || WhereType::AND === $whereType) {
+            $this->queryBuilder->expects($this->once())
+                ->method('andWhere')
+                ->with($isBetween);
+        } else {
+            $this->queryBuilder->expects($this->once())
+                ->method('orWhere')
+                ->with($isBetween);
+        }
+
+        $this->typecaster->expects($this->exactly(2))
+            ->method('typecast')
+            ->withConsecutive(
+                [$this->metadata, $fieldName, $from, $formatType, []],
+                [$this->metadata, $fieldName, $to, $formatType, []],
+            )->willReturnOnConsecutiveCalls(
+                $from,
+                $to
+            );
+
+        $this->queryBuilder->expects($this->exactly(2))
+            ->method('setParameter')
+            ->withConsecutive(
+                [$fromParam, $from],
+                [$toParam, $to]
+            );
+
+        $filter->filter($this->queryBuilder, $this->metadata, $criteria);
+    }
+
+    /**
+     * @return array
+     */
+    public function getFilterIsBetweenData(): array
+    {
+        return [
+            [
+                [
+                    'to' => '2021-01-01 00:00:00',
+                    'from' => '2021-02-01 00:00:00',
+                ]
+            ],
+        ];
+    }
+}
