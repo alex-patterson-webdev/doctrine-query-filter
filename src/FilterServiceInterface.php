78f4d6ad65a1c7e80583e6b31db3e9c65d3b5708
Alex Patterson <alex.patterson.webdev@gmail.com>
2021-04-28 21:13:21 +0000

    Add generic FilterServiceInterface which can be applied to services which require filtering abilities

---
+++ src/FilterServiceInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arp\DoctrineQueryFilter;
+
+/**
+ * A service which will perform filtering
+ *
+ * @author  Alex Patterson <alex.patterson.webdev@gmail.com>
+ * @package Arp\DoctrineQueryFilter
+ */
+interface FilterServiceInterface
+{
+    /**
+     * @param QueryFilterManagerInterface $filterManager
+     * @param array                       $criteria
+     *
+     * @return iterable
+     *
+     * @throws \Exception
+     */
+    public function filter(QueryFilterManagerInterface $filterManager, array $criteria): iterable;
+}
