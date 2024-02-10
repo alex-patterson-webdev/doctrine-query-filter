0c64994f5c9f9ae62a2699e7267ecfe72e99db81
Alex <alex@alpat.co.uk>
2024-02-10 22:42:59 +0000

    Modify the QueryFilterManagerInterface and QueryFilterManager implementation class to allow for public calls to methods applyFilter(), createFilter(), applySort() and createSort(). This allows custom filter classses to utilise the existing filters internally

--- src/QueryFilterManagerInterface.php
+++ src/QueryFilterManagerInterface.php
@@ -5,18 +5,15 @@ declare(strict_types=1);
 namespace Arp\DoctrineQueryFilter;
 
 use Arp\DoctrineQueryFilter\Exception\QueryFilterManagerException;
+use Arp\DoctrineQueryFilter\Filter\FilterInterface;
+use Arp\DoctrineQueryFilter\Metadata\MetadataInterface;
+use Arp\DoctrineQueryFilter\Sort\SortInterface;
 use Doctrine\ORM\QueryBuilder as DoctrineQueryBuilder;
 
 interface QueryFilterManagerInterface
 {
     /**
-     * Apply the query filters to the provided query builder instance
-     *
-     * @param DoctrineQueryBuilder|QueryBuilderInterface $queryBuilder
-     * @param string $entityName
-     * @param array<mixed> $criteria
-     *
-     * @return DoctrineQueryBuilder
+     * Generate a query builder instance constrained by the provided criteria
      *
      * @throws QueryFilterManagerException
      */
@@ -25,4 +22,40 @@ interface QueryFilterManagerInterface
         string $entityName,
         array $criteria
     ): DoctrineQueryBuilder;
+
+    /**
+     * Apply the query filters to the provided query builder instance
+     *
+     * @throws QueryFilterManagerException
+     */
+    public function applyFilter(
+        QueryBuilderInterface $queryBuilder,
+        MetadataInterface $metadata,
+        array|FilterInterface $data
+    ): void;
+
+    /**
+     * Create a new filter instance by the provided name and options
+     *
+     * @throws QueryFilterManagerException
+     */
+    public function createFilter(string $name, array $options = []): FilterInterface;
+
+    /**
+     * Apply the query sort to the provided query builder instance
+     *
+     * @throws QueryFilterManagerException
+     */
+    public function applySort(
+        QueryBuilderInterface $queryBuilder,
+        MetadataInterface $metadata,
+        array|SortInterface $data
+    ): void;
+
+    /**
+     * Create a new sort instance by the provided name and options
+     *
+     * @throws QueryFilterManagerException
+     */
+    public function createSort(string $name, array $options = []): SortInterface;
 }
